344b45922093f6a1b0ef42000f32d76b8f895f46
ericpham <phamwangthang@gmail.com>
2024-02-21 00:14:57 +0100

    css

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -23,7 +23,7 @@ const WeatherApp = () => {
 		<div className="weather-image">
 			<img src={cloudy_icon} alt="" />
 		</div>
-		<div className="weather-temp">24C</div>
+		<div className="weather-temp">24°C</div>
 		<div className="weather-location">London</div>
 		<div className="data-container">
 			<div className="element">
